95bd60dd40ea7d73eaa77fb632a8c0b655f491da
tyaa <tyaa@ukr.net>
2017-08-09 16:26:50 +0300

    add action get-data-by-section

---
+++ ProstoASP.Domain/Entity/PageData.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProstoASP.Domain.Entity
+{
+    public class PageData
+    {
+        private int id;
+        //название раздела сайта, его "ключ"
+        private String section;
+        //заголовок раздела для отображения
+        private String title;
+        //содержимое описания раздела
+        private String content;
+
+        public PageData() { }
+
+        public PageData(
+                String _section
+                , String _title
+                , String _content
+            )
+        {
+
+            section = _section;
+            title = _title;
+            content = _content;
+        }
+
+        public String getTitle()
+        {
+            return title;
+        }
+
+        public void setTitle(String title)
+        {
+            this.title = title;
+        }
+
+        public String getContent()
+        {
+            return content;
+        }
+
+        public void setContent(String content)
+        {
+            this.content = content;
+        }
+
+        public int getId()
+        {
+            return id;
+        }
+
+        public String getSection()
+        {
+            return section;
+        }
+
+        public void setSection(String section)
+        {
+            this.section = section;
+        }
+    }
+}
--- ProstoASP.Web/Controllers/DefaultController.cs
+++ ProstoASP.Web/Controllers/DefaultController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using ProstoASP.Domain.Entity;
+
 namespace ProstoASP.Web.Controllers
 {
     public class DefaultController : Controller
@@ -12,6 +14,47 @@ namespace ProstoASP.Web.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public JsonResult DoAction()
+        {
+            dynamic result = new { };
+            if (Request["action"] != null)
+            {
+                String actionString = Request["action"];
+                switch (actionString)
+                {
+                    case "get-data-by-section":
+                        {
+                            if (Request["section"] != null)
+                            {
+                                PageData pageData =
+                                    new PageData(
+                                        "home"
+                                        , "Главная"
+                                        , "Зачем усложнять себе жизнь, если все можно просто поручить профи?.."
+                                    );
+                                result =
+                                    new {
+                                        id = pageData.getId()
+                                        , title = pageData.getTitle()
+                                        , content = pageData.getContent()
+                                    };
+                            }
+                            break;
+                        }
+                    default:
+                        break;
+                }
+            }
+            //
+            /*var result =
+                from item in mRepository.Project
+                where (item.Id == _projectId)
+                select new { item.Id, item.User, item.Width, item.Height };
+            return Json(result, JsonRequestBehavior.AllowGet);*/
+            return Json(result);
         }
     }
 }
