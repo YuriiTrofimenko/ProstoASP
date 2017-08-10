1669719d37dd555ba098f7d62761d311adadd93a
tyaa <tyaa@ukr.net>
2017-08-10 16:08:40 +0300

    use LiteDB

---
+++ ProstoASP.Domain/Abstract/IProjectRepository.cs
@@ -0,0 +1,16 @@
+﻿using LiteDB;
+using ProstoASP.Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProstoASP.Domain.Abstract
+{
+    public interface IProjectRepository
+    {
+        PageData SavePageData(PageData _pageData);
+        LiteCollection<PageData> PageData { get; }
+    }
+}
---
+++ ProstoASP.Domain/Concrete/LiteDBProjectRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ProstoASP.Domain.Abstract;
+using ProstoASP.Domain.Entity;
+using LiteDB;
+
+namespace ProstoASP.Domain.Concrete
+{
+    public class LiteDBProjectRepository : IProjectRepository
+    {
+        public LiteDatabase db;
+
+        public LiteDBProjectRepository()
+        {
+            db = new LiteDatabase(@"Prosto.db");
+        }
+
+        LiteCollection<PageData> IProjectRepository.PageData
+        {
+            get => db.GetCollection<PageData>("PageDatas");
+        }
+
+        PageData IProjectRepository.SavePageData(PageData _pageData)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
